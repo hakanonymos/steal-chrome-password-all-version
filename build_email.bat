dd6cf67cd39e9c8fe675a19af4ba1cccf4ebdd49
hakanonymos <30985149+hakanonymos@users.noreply.github.com>
2020-08-18 15:37:12 +0000

    Add files via upload

--- build_email.bat
+++ build_email.bat
@@ -13,7 +13,7 @@ pip3 install cryptography
 pip3 install pyinstaller
 
 
-pyinstaller --clean --hidden-import=pyttsx3.drivers --hidden-import=pyttsx3.drivers.sapi5 --onefile --noconsole --i icone.ico email.py
+pyinstaller --clean --hidden-import=pyttsx3.drivers --hidden-import=pyttsx3.drivers.sapi5 --onefile --noconsole --i icone.ico my_email.py
 
 del /s /q /f build.spec
 rmdir /s /q __pycache__
